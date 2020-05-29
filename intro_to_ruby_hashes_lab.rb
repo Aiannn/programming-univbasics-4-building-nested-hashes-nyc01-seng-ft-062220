43aa00302b0c1a0bfdd3eb84e70106b6708852c0
Aian Soktoev <soktoevayan@gmail.com>
2020-05-29 22:57:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,9 +38,9 @@ def monopoly_with_third_tier
       },
       :names => {
         :reading_railroad => {},
-        :pennsylvania_railroad = {},
-        :b_and_o_railroad = {},
-        :shortline_railroad = {}
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
       }
     }
   }
@@ -64,13 +64,13 @@ def monopoly_with_fourth_tier
         :reading_railroad => {
           :mortgage_value => 100
         },
-        :pennsylvania_railroad = {
+        :pennsylvania_railroad => {
           :mortgage_value => 200
         },
-        :b_and_o_railroad = {
+        :b_and_o_railroad => {
           :mortgage_value => 400
         },
-        :shortline_railroad = {
+        :shortline_railroad => {
           :mortgage_value => 800
         }
       }
