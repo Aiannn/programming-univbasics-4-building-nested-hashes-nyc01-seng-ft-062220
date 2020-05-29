55811f9e562e70fbbc9324a4347fcc7d52147da1
Aian Soktoev <soktoevayan@gmail.com>
2020-05-29 22:52:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,8 +30,15 @@ def monopoly_with_third_tier
   a = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {},
-      :names => {}
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_piece_owned => 50,
+        :three_piece_owned => 100,
+        :four_pieces_owned => 200
+      },
+      :names => {
+        
+      }
     }
   }
 end
